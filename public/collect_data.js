4963a3416cd577145b33b6da8e4cd4e5423add41
mrzhuzhe <466648706@qq.com>
2022-03-02 20:26:33 +0800

    release predict

---
+++ public/collect_data.js
@@ -0,0 +1,3 @@
+<button id="left" onmousedown="collect(0)" onmouseup="collect(null)">Left</button>
+<button id="right" onmousedown="collect(1)" onmouseup="collect(null)">Right</button>
+<button id="noise" onmousedown="collect(2)" onmouseup="collect(null)">Noise</button>
---
+++ public/index.js
@@ -0,0 +1,21 @@
+let recognizer;
+
+function predictWord() {
+ // Array of words that the recognizer is trained to recognize.
+ const words = recognizer.wordLabels();
+ recognizer.listen(({scores}) => {
+   // Turn scores into a list of (score,word) pairs.
+   scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+   // Find the most probable word.
+   scores.sort((s1, s2) => s2.score - s1.score);
+   document.querySelector('#console').textContent = scores[0].word;
+ }, {probabilityThreshold: 0.75});
+}
+
+async function app() {
+ recognizer = speechCommands.create('BROWSER_FFT');
+ await recognizer.ensureModelLoaded();
+ predictWord();
+}
+
+app();
