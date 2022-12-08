e145c86e1db87922c9347576e6499f0424cf5ff2
mrzhuzhe <466648706@qq.com>
2022-12-08 16:06:43 +0800

    function pointer  template

--- cpp_test/src/templateTest.cpp
+++ cpp_test/src/templateTest.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <vector>
 #include <memory>
+//#include <ofstram>
+
 using std::vector;
 using namespace std;
 
@@ -39,11 +41,11 @@ private:
     void check(size_type i, const std::string &msg) const;
 };
 
-template <typename T> Blob2<T>::Blob2() {
+template <typename T> Blob2<T>::Blob2(): data(std::make_shared<std::vector<T>>()) {
 
 }
 
-template <typename T> Blob2<T>::Blob2(std::initializer_list<T> il) {
+template <typename T> Blob2<T>::Blob2(std::initializer_list<T> il): data(std::make_shared<std::vector<T>>(il)) {
 
 }
 
@@ -51,6 +53,78 @@ template <typename T> Blob2<T>::Blob2(std::initializer_list<T> il) {
 template <typename T>  void Blob2<T>::check(size_type i, const std::string &msg) const {
     if (i >= data->size())
         throw std::out_of_range(msg);
+}
+
+template <typename T> T& Blob2<T>::back()
+{
+    check(0, "back on empty blob");
+    return data->back();
+}
+
+template <typename T> T& Blob2<T>::operator[] (size_type i)
+{
+    check(i, "subscript out of range");
+    return (*data)[i];
+}
+
+template <typename T> void Blob2<T>::pop_back()
+{
+    check(0, "pop back on empty Blob");
+    data->pop_back();
+}
+
+//  pointer to template
+template <typename T> class BlobPtr
+{
+    public:
+        BlobPtr(): curr(0) {}
+        BlobPtr(Blob2<T> &a, size_t sz = 0): wptr(a.data), curr(sz) {}
+        T& operator*() const 
+        {
+            auto p = check(curr, "dereference past end");
+            return (*p)[curr];
+        }
+        BlobPtr operator++();
+        BlobPtr operator--();
+    private:
+        std::shared_ptr<std::vector<T>>
+            check(std::size_t, const std::string&) const;
+        std::weak_ptr<std::vector<T>> wptr;
+        std::size_t curr;
+};
+
+
+template<typename T> using BlobRename = Blob2<T>;
+using BlobIntRename = Blob2<int>;
+
+
+template <typename T> ostream &print(ostream &os, const T &obj)
+{
+    return os << obj << endl;
+}
+
+/*
+    function pointer 
+*/
+void g(int &&i, int& j)
+{
+    cout << i << " " << j << endl;
+}
+
+void f(int v1, int &v2)
+{
+    cout << v1 << " " << ++v2 << endl;
+} 
+
+template <typename F, typename T1, typename T2> void flip(F f, T1 t1, T2 t2)
+{
+    f(t2, t1);
+}
+
+template <typename F, typename T1, typename T2> void flip2(F f, T1 &&t1, T2 &&t2)
+{
+    //f(t2, t1);
+    f(std::forward<T2>(t2), std::forward<T1>(t1));
 }
 
 int main(int argc, char **argv) {
@@ -67,10 +141,24 @@ int main(int argc, char **argv) {
 
     Blob2<int> ia;
     Blob2<int> ia2 = {0, 1, 2, 3, 4};
+    
+    //ia2.back();
 
     Blob2<string> name;
-
+    Blob2<string> name2 = { "a", "an", "the" };
     Blob2<double> prices;
 
+    BlobRename<int> ia3 = {   0, 2, 3, 4  };
+    BlobIntRename ia4 = { 5, 4, 3 };
+
+    print(cout, 42);
+    //ofstram f("output");
+    //print(f, 10);
+
+    int i = 123;
+    f(422, i);
+    flip(f, i, 42);
+
+    flip2(g, i, 42);    // error: can’t initialize int&& from an lvalue
     return 0;
 }
