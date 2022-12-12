c2d26ca63f13754ef770abff20068120c8b08d15
mrzhuzhe <466648706@qq.com>
2022-12-12 17:48:43 +0800

    cuda hello world

---
+++ cuda_test/test01.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void print_from_gpu(void) {
+    printf("Hello world from thread [%d, %d] from devices\n ", threadIdx.x, blockIdx.x);
+}
+
+int main(void) {
+    printf("Hello world from host! \n");
+    print_from_gpu<<<1,1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
