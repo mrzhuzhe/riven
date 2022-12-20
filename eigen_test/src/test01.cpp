f8c6d70322fe6d81810208c689b8f0d405ca1e79
mrzhuzhe <466648706@qq.com>
2022-12-20 19:00:06 +0800

    cmake eigen

--- eigen_test/src/test01.cpp
+++ eigen_test/src/test01.cpp
@@ -2,11 +2,16 @@
 #include <Eigen/Dense>
  
 using namespace std;
+using namespace Eigen;
 int main()
 {
-  Eigen::Matrix2d mat;
+  //Eigen::Matrix2d mat;
+  MatrixXd mat(2,2);
+
   mat << 1, 2,
          3, 4;
+ 
+  //cout << "" << mat.coeff()       << endl;
   cout << "Here is mat.sum():       " << mat.sum()       << endl;
   cout << "Here is mat.prod():      " << mat.prod()      << endl;
   cout << "Here is mat.mean():      " << mat.mean()      << endl;
