ffd4bf5d4e24c2282bd70fa25ca91a694cf659ee
mrzhuzhe <466648706@qq.com>
2023-03-04 16:57:15 +0800

    jacobi iter

--- cuda_practice/src/blur.cu
+++ cuda_practice/src/blur.cu
@@ -62,6 +62,87 @@ __global__ void parallel_xblur(float *out, float const *in, int nx, int ny){
     out[y*nx+x] = sum / nblur;
 }
 
+template <int blockSize>
+__global__ void parallel_jacobi(float *out, float const *in, int nx, int ny){
+    int x = blockIdx.x * blockSize + threadIdx.x;
+    int y = blockIdx.y * blockSize + threadIdx.y;
+    if (x>=nx || y>=ny) return;
+    float res = (
+        in[std::min(y+1, ny-1)*nx + x]
+        +in[std::max(y-1, 0)*nx +x]
+        +in[y*nx+std::min(x+1, nx-1)]
+        +in[y*nx+std::max(x-1, 0)]
+    ) / 4;
+    out[y*nx+x] = res;
+}
+
+template <int iters, int blockSize>
+__global__ void parallel_jacobi_kernel(float *out, float const *in, int nx, int ny){
+    int blockX = blockIdx.x;
+    int blockY = blockIdx.y;
+    int threadX = threadIdx.x;
+    int threadY = threadIdx.y;
+    constexpr int chunkSize = blockSize - iters * 2;
+    int globalX = blockX * chunkSize - iters + threadX;
+    int globalY = blockY * chunkSize - iters + threadX;
+
+    __shared__ float mem[2][blockSize+2][blockSize+2];
+    int clampedX = std::min(std::max(globalX, 0), nx-1);
+    int clampedY = std::min(std::max(globalY, 0), ny-1);
+
+    mem[0][1+threadY][1+threadX] = in[nx*clampedY + clampedX];
+
+    if (threadY == 0){
+        int clampedYn = std::min(std::max(blockY * chunkSize - iters - 1, 0), ny - 1);
+        mem[0][0][1+threadX] = in[nx*clampedYn + clampedX];
+        int clampedYp = std::min(std::max(blockY * chunkSize - iters + blockSize, 0), ny-1);
+        mem[0][1+blockSize][1+threadX] = in[nx*clampedYp + clampedX];
+    }
+
+    if (threadX == 0){
+        int clampedXn = std::min(std::max(blockX * chunkSize - iters - 1, 0), nx - 1);
+        mem[0][0][1+threadY] = in[nx*clampedY + clampedXn];
+        int clampedXp = std::min(std::max(blockX * chunkSize - iters + blockSize, 0), nx-1);
+        mem[0][1+threadY][1+blockSize]= in[nx*clampedY + clampedXp];
+    }
+
+    __syncthreads();
+
+    for (int stage = 0; stage < iters; stage += 2){
+#pragma unroll
+        for (int phase = 0; phase < 2; phase ++){
+            mem[1-phase][1+threadY][1+threadX] = 
+                ( mem[phase][1 + threadY + 1][1+ threadX]
+                + mem[phase][1 + threadY - 1][1+ threadX]
+                + mem[phase][1 + threadY][1 + threadX + 1]
+                + mem[phase][1 + threadY][1 + threadX - 1]
+                ) /4;
+            __syncthreads();
+        }
+    }
+
+    if (threadX >= iters && threadX < blockSize - iters){
+        if (threadY >= iters && threadY < blockSize - iters){
+            if (globalX < nx && globalY < ny){
+                out[globalY*nx + globalX] = mem[0][1+threadY][1+threadX];
+            }
+        }
+    }
+
+
+
+}
+
+template <int iters, int blockSize>
+void parallel_jacobi2(float *out, float const *in, int nx, int ny){
+    constexpr int chunkSize = blockSize - iters * 2;
+    static_assert(chunkSize > 0 && iters % 2 == 0);
+    parallel_jacobi_kernel<iters, blockSize>
+    <<<dim3((nx+chunkSize-1)/ chunkSize, (ny+chunkSize-1)/chunkSize, 1), dim3(blockSize, blockSize, 1)>>>
+    (out, in, nx, ny);
+}
+
+
 int main(){
     std::vector<float, CudaAllocator<float>> in;
     std::vector<float, CudaAllocator<float>> out;
@@ -70,12 +151,29 @@ int main(){
     out.resize(in.size());
 
     Tik();
+    /*
     parallel_xblur<32, 32><<<dim3((nx+31)/32, (ny+31)/32, 1), dim3(32, 32, 1)>>>
     (out.data(), in.data(), nx, ny);
+    */
+    /*
+    for (int step = 0; step < 1024; step++){
+        parallel_jacobi<32><<<dim3((nx+31)/32, (ny+31)/32, 1), dim3(32, 32, 1)>>>
+            (out.data(), in.data(), nx, ny);
+        std::swap(out, in);
+    }
+    */
+    constexpr int iters = 4;
+    for (int step = 0; step < 1024; step += iters){
+        parallel_jacobi2<iters, 32>
+            (out.data(), in.data(), nx, ny);
+        std::swap(out, in);
+    }
+
+
     cudaDeviceSynchronize();
     Tok("blur");
     write_image(out, nx, ny, 1, "./outputs/out.png");
-    system("display ./outputs/out.png &");
+    //system("display ./outputs/out.png &");
 
     return 0;
 }
