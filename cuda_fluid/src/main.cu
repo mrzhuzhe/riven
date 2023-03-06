4460c07071d2639c293ede511533b52c73d6669c
mrzhuzhe <466648706@qq.com>
2023-03-06 21:39:16 +0800

    template wrong

--- cuda_fluid/src/main.cu
+++ cuda_fluid/src/main.cu
@@ -1,5 +1,8 @@
 #include <cstdio>
 #include <cuda_runtime.h>
+#include <memory>
+#include <iostream>
+#include <vector>
 
 struct DisableCopy {
     DisableCopy() = default;
@@ -303,11 +306,31 @@ __global__ void sumloss_kernel(CudaSurfaceAccessor<float> sufDiv, float *sum, un
     atomicAdd(sum, div*div);
 }
 
+
+template <int phase>
+__global__ void rbgs_kernel(CudaSurfaceAccessor<float> sufPre, CudaSurfaceAccessor<float> sufDiv, unsigned int n){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    if (x >= n || y >=n || z>=n ) return;
+    if (( x + y + z) % 2 != phase) return;
+
+    float pxp = sufPre.read<cudaBoundaryModeClamp>(x+1, y, z);
+    float pxn = sufPre.read<cudaBoundaryModeClamp>(x-1, y, z);
+    float pyp = sufPre.read<cudaBoundaryModeClamp>(x, y+1, z);
+    float pyn = sufPre.read<cudaBoundaryModeClamp>(x, y-1, z);
+    float pzp = sufPre.read<cudaBoundaryModeClamp>(x, y, z+1);
+    float pzn = sufPre.read<cudaBoundaryModeClamp>(x, y, z-1);
+    float div = sufDiv.read(x, y, z);
+    float preNext = (pxp + pxn + pyp + pyn + pzp + pzn - div) * (1.f / 6.f);
+    sufPre.write(preNext, x, y, z);
+}
+
 void smooth(CudaSurface<float> *v, CudaSurface<float> *f, unsigned int lev, int times = 4){
-    usigned int tn = sizes[lev];
+    unsigned int tn = sizes[lev];
     for (int step = 0; step < times; step++){
-        rgbs_kernel<0><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
-        rgbs_kernel<1><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
+        rbgs_kernel<0><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
+        rbgs_kernel<1><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
     }
 }
 
@@ -330,7 +353,116 @@ void vcycle(unsigned int lev, CudaSurface<float> *v, CudaSurface<float> *f){
     smooth(v, f, lev);
 }
 
+
+__global__ void residual_kernel(CudaSurfaceAccessor<float> sufRes, CudaSurfaceAccessor<float> sufPre, CudaSurfaceAccessor<float> sufDiv, unsigned int n){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    if (x >= n || y >=n || z>=n ) return;
+
+    float pxp = sufPre.read<cudaBoundaryModeClamp>(x+1, y, z);
+    float pxn = sufPre.read<cudaBoundaryModeClamp>(x-1, y, z);
+    float pyp = sufPre.read<cudaBoundaryModeClamp>(x, y+1, z);
+    float pyn = sufPre.read<cudaBoundaryModeClamp>(x, y-1, z);
+    float pzp = sufPre.read<cudaBoundaryModeClamp>(x, y, z+1);
+    float pzn = sufPre.read<cudaBoundaryModeClamp>(x, y, z-1);
+    float pre = sufPre.read(x, y, z);
+    float div = sufDiv.read(x, y, z);
+    float res = (pxp + pxn + pyp + pyn + pzp + pzn - div - 6.f * pre );
+    sufRes.write(res, x, y, z);
+}
+
+
+__global__ void restrict_kernel(CudaSurfaceAccessor<float> sufPreNext, CudaSurfaceAccessor<float> sufPre, CudaSurfaceAccessor<float> sufDiv, unsigned int n){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    if (x >= n || y >=n || z>=n ) return;
+
+    float ooo = sufPre.read<cudaBoundaryModeClamp>(x*2, y*2, z*2);
+    float ioo = sufPre.read<cudaBoundaryModeClamp>(x*2+1, y*2, z*2);
+    float oio = sufPre.read<cudaBoundaryModeClamp>(x*2, y*2+1, z*2);
+    float iio = sufPre.read<cudaBoundaryModeClamp>(x*2+1, y*2+1, z*2);
+    float ooi = sufPre.read<cudaBoundaryModeClamp>(x*2, y*2, z*2+1);
+    float ioi = sufPre.read<cudaBoundaryModeClamp>(x*2+1, y*2, z*2+1);
+    float oii = sufPre.read<cudaBoundaryModeClamp>(x*2, y*2+1, z*2+1);
+    float iii = sufPre.read<cudaBoundaryModeClamp>(x*2+1, y*2+1, z*2+1);
+
+    float preNext = (ooo + ioo + oio + iio + ooi + ioi + oii + iii);
+    sufPreNext.write(preNext, x, y, z);
+}
+
+
+__global__ void restrict_kernel(CudaSurfaceAccessor<float> sufPre, unsigned int n){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    if (x >= n || y >=n || z>=n ) return;
+    sufPre.write(0.f, x, y, z);    
+}
+
+
+__global__ void prolongate_kernel(CudaSurfaceAccessor<float> sufPreNext, CudaSurfaceAccessor<float> sufPre, unsigned int n){
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    if (x >= n || y >=n || z>=n ) return;
+    
+    float preDelta = sufPre.read(x, y, z) * (0.5f / 8.f);
+#pragma unroll
+    for (int dz = 0; dz < 2; dz++}{
+#pragma unroll
+        for (int dy = 0; dy < 2; dy++){
+#pragma unroll
+            for (int dx = 0; dx < 2; dx++){
+                float preNext = sufPreNext.read(cudaBoundaryModeZero)(x*2+dx, y*2+dy, z*2+dz);
+                preNext += preDelta;
+                sufPreNext.write<cudaBoundaryModeZero>(preNext, x*2+dx, y*2+dy, z*2+dz);
+            }
+        }
+    }
+}
+
 int main(){
-   
+    unsigned int n = 128;
+    SmokeSim sim(n);
+    {
+        std::vector<float4> cpu(n*n*n);
+        for (int z = 0; z < n; z++){
+            for (int y = 0; y < n; y++){
+                for (int x = 0; x < n; x++){
+                    float den = std::hypot(x - (int)n/2, y - (int)n/2, z - (int)n/2) < n / 6 ? 1.f : 0.f;
+                    cpu[x+n*(y+n*z)] = make_float4(den, 0.f, 0.f, 0.f);
+                }
+            }
+        }
+        sim.clr -> copyIn(cpu.data());
+    }
+
+    {
+        std::vector<float4> cpu(n*n*n);
+        for (int z = 0; z < n; z++){
+            for (int y = 0; y < n; y++){
+                for (int x = 0; x < n; x++){
+                    float vel = std::hypot(x - (int)n/2, y - (int)n/2, z - (int)n/2) < n / 6 ? 0.9f : 0.f;
+                    cpu[x+n*(y+n*z)] = make_float4(0.f, 0.f, vel, 0.f);
+                }
+            }
+        }
+        sim.vel -> copyIn(cpu.data());
+    }
+
+    std::vector<std::thread> tpool;
+    for (int frame = 1; frame <= 250; frame++){
+        std::vector<float4> cpu(n*n*n);
+        sim.clr->copyOut(cpu.data());
+        tpool.push_back(std::thread([cpu = std::move(cpu), frame, n]{
+            writevdb<float, 1>("./outputs/test" + std::to_string(1000+frame).substr(1)+".vdb", cpu.data(), n, n, n, sizeof(float4));
+        }));
+        printf("frame=%d, loss=%f\n", frame, sim.calc_loss());
+        sim.step();
+    }
+
+    for (auto &t: tpool) t.join();
     return 0;
 }
