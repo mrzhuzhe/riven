a03fce33342d753cfb0d48c7b86f179fbaf892d9
mrzhuzhe <466648706@qq.com>
2023-03-07 18:25:55 +0800

    vdb

--- cuda_fluid/src/main.cu
+++ cuda_fluid/src/main.cu
@@ -3,12 +3,13 @@
 #include <memory>
 #include <iostream>
 #include <vector>
+#include <thread>
 
 struct DisableCopy {
     DisableCopy() = default;
     DisableCopy(DisableCopy const &) = delete;
     DisableCopy &operator=(DisableCopy const &) = delete;
-}
+};
 
 template <class T>
 struct  CudaArray : DisableCopy
@@ -57,7 +58,7 @@ struct CudaSurfaceAccessor {
         return surf3Dread<T>(m_cuSuf, x*sizeof(T), y, z, mode);
     }
     template <cudaSurfaceBoundaryMode mode = cudaBoundaryModeTrap>
-    __device__ __forceinline__ T write(int x, int y, int z) const {
+    __device__ __forceinline__ T write(T val, int x, int y, int z) const {
         return surf3Dwrite<T>(val, m_cuSuf, x*sizeof(T), y, z, mode);
     }
 };
@@ -65,7 +66,7 @@ struct CudaSurfaceAccessor {
 template <class T>
 struct  CudaSurface : CudaArray<T>
 {
-    cudaSurfaceObject_t m_cuSur{};
+    cudaSurfaceObject_t m_cuSuf{};
     explicit CudaSurface(uint3 const &_dim): CudaArray<T>(_dim){
         cudaResourceDesc resDesc{};
         resDesc.resType = cudaResourceTypeArray;
@@ -78,10 +79,10 @@ struct  CudaSurface : CudaArray<T>
     }
 
     CudaSurfaceAccessor<T> accessSurface() const {
-        return {m_cuSuf}
+        return {m_cuSuf};
     }
     ~CudaSurface(){
-        cudaDEstroySurfaceObject(m_cuSuf);
+        cudaDestroySurfaceObject(m_cuSuf);
     }
 
 };
@@ -90,7 +91,7 @@ struct  CudaSurface : CudaArray<T>
 template <class T>
 struct CudaTextureAccessor {
     cudaTextureObject_t m_cuTex;
-    __device__ __forceinline__ T sample(float x, float y, foat z) const {
+    __device__ __forceinline__ T sample(float x, float y, float z) const {
         return tex3D<T>(m_cuTex, x, y, z);
     }
 };
@@ -138,41 +139,6 @@ struct  CudaTexture : CudaSurface<T>{
 
 };
 
-struct SmokeSim : DisableCopy
-{
-    unsinged int n;
-    std::unique_ptr<CudaSurface<float4>> loc;
-    std::unique_ptr<CudaTexture<float4>> vel;
-    std::unique_ptr<CudaTexture<float4>> velNext;
-    std::unique_ptr<CudaTexture<float4>> clr;
-    std::uniqur_ptr<CudaTexture<float4>> clrNext;
-
-    std::unique_ptr<CudaSurface<float>> div;
-    std::unique_ptr<CudaSurface<float>> pre;
-    std::vector<std::unique_ptr<CudaSurface<float>>> res;
-    std::vector<std::unique_ptr<CudaSurface<float>>> res2;
-    std::vector<std::unique_ptr<CudaSurface<float>>> err2;
-    std:vector<unsigned int> sizes;
-
-    explicit SmokeSim(unsigned int _n, unsigned int _n0 = 16)
-    :n(_n), loc(std::make_unique<CudaSurface<float4>>(uint3{n, n, n}))
-    , vel(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
-    , velNext(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
-    , clr(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
-    , clrNext(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
-    , div(std::make_unique<CudaSurface<float>>(uint3{n, n, n}))
-    , pre(std::make_unique<CudaSurface<float>>(uint3{n, n, n}))
-    {
-        unsigned int tn;
-        for (tn = n; tn >= _n0; tn /=2){
-            res.push_back(std::make_unique<CudaSurface<float>>(uint3{tn, tn, tn}));
-            res2.push_back(std::make_unique<CudaSurface<float>>(uint3{tn/2, tn/2, tn/2}));
-            err2.push_back(std::make_unique<CudaSurface<float>>(uint3{tn/2, tn/2, tn/2}));
-            sizes.push_back(tn);
-        }
-    }
-};
-
 __global__ void advect_kernel(CudaTextureAccessor<float4> texVel, CudaSurfaceAccessor<float4> sufloc, unsigned int n){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
@@ -189,7 +155,7 @@ __global__ void advect_kernel(CudaTextureAccessor<float4> texVel, CudaSurfaceAcc
     float3 vel2 = sample(texVel, loc-0.5f*vel1);
     float3 vel3 = sample(texVel, loc-0.5f*vel2);
     loc -= (2.f/9.f) * vel1 + (1.f/3.f)*vel2 + (4.f/9.f)*vel3;
-    surLoc.write(make_float4(loc.x, loc.y, loc.z, 0.f), x, y, z);
+    sufLoc.write(make_float4(loc.x, loc.y, loc.z, 0.f), x, y, z);
 
 }
 
@@ -204,21 +170,7 @@ __global__ void resample_kernel(CudaTextureAccessor<float4> sufLoc, CudaSurfaceA
     sufClrNext.write(clr, x, y, z);
 }
 
-void advection() {
-    advect_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(vel->accessTexture(), loc->accessSurface(), n);
-    resample_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(loc->accessSurface(), clr->accessTexture(), clrNext->accessSurface(), n);
-    resample_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(loc->accessSurface(), vel->accessTexture(), velNext->accessSurface(), n);
 
-    std::swap(vel, velNext);
-    std::swap(clr, clrNext);
-}
-
-void step(int times = 16){
-    for (int step = 0; step < times, step++){
-        projection();
-        advection();
-    }
-}
 
 __global__ void divergence_kernel(CudaSurfaceAccessor<float4> sufVel, CudaSurfaceAccessor<float> surDiv, unsigned int n){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
@@ -230,13 +182,13 @@ __global__ void divergence_kernel(CudaSurfaceAccessor<float4> sufVel, CudaSurfac
     float vyp = sufVel.read<cudaBoundaryModeZero>(x, y+1, z).y;
     float vzp = sufVel.read<cudaBoundaryModeZero>(x, y, z+1).z;
     float vxn = sufVel.read<cudaBoundaryModeZero>(x-1, y, z).x;
-    float vxn = sufVel.read<cudaBoundaryModeZero>(x, y-1, z).y;
-    float vxn = sufVel.read<cudaBoundaryModeZero>(x, y, z-1).z;
+    float vyn = sufVel.read<cudaBoundaryModeZero>(x, y-1, z).y;
+    float vzn = sufVel.read<cudaBoundaryModeZero>(x, y, z-1).z;
     float div = (vxp - vxn + vyp - vyn + vzp - vzn) * 0.5f;
     sufDiv.write(div, x, y, z);
 }
 
-__global__ void jacobi_kernel(CudaSurfaceAccessor<float> sufDiv, CudaSurfaceAccessor<float> surPre, CudaSurfaceAccessor<float> surPreNext, unsigned int n){
+__global__ void jacobi_kernel(CudaSurfaceAccessor<float> sufDiv, CudaSurfaceAccessor<float> surPre, CudaSurfaceAccessor<float> sufPreNext, unsigned int n){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     int z = threadIdx.z + blockDim.z * blockIdx.z;
@@ -260,41 +212,19 @@ __global__ void subgradient_kernel(CudaSurfaceAccessor<float> sufPre, CudaSurfac
     if (x >= n || y >=n || z>=n ) return;
     if (sufBound.read(x, y, z) < 0) return;
 
-    float vxp = sufVel.read<cudaBoundaryModeZero>(x+1, y, z).x;
-    float vyp = sufVel.read<cudaBoundaryModeZero>(x, y+1, z).y;
-    float vzp = sufVel.read<cudaBoundaryModeZero>(x, y, z+1).z;
-    float vxn = sufVel.read<cudaBoundaryModeZero>(x-1, y, z).x;
-    float vxn = sufVel.read<cudaBoundaryModeZero>(x, y-1, z).y;
-    float vxn = sufVel.read<cudaBoundaryModeZero>(x, y, z-1).z;
-    float vel = sufVel.read(x, y,z);
+    float pxp = sufVel.read<cudaBoundaryModeZero>(x+1, y, z).x;
+    float pyp = sufVel.read<cudaBoundaryModeZero>(x, y+1, z).y;
+    float pzp = sufVel.read<cudaBoundaryModeZero>(x, y, z+1).z;
+    float pxn = sufVel.read<cudaBoundaryModeZero>(x-1, y, z).x;
+    float pyn = sufVel.read<cudaBoundaryModeZero>(x, y-1, z).y;
+    float pzn = sufVel.read<cudaBoundaryModeZero>(x, y, z-1).z;
+    float4 vel = sufVel.read(x, y,z);
     vel.x -= (pxp - pxn) * 0.5f;
     vel.y -= (pyp - pyn) * 0.5f;
     vel.z -= (pzp - pzn) * 0.5f;
     sufVel.write(vel, x, y, z);
 }
 
-void projection (int times = 400){
-    divergence_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(vel->accessSurface(), div->accessSurface(), n);
-
-    for (int step = 0; step < times, step++){
-        jacobi_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(div->accessSurface(), pre->accessSurface(), preNext->accessSurface(), n);
-        std::swap(pre, preNext);
-    }
-
-    subgradient_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(pre->accessSurface(), vel->accessSurface(), n);
-}
-
-float calc_loss(){
-    divergence_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(vel->accessSurface(), div->accessSurface(), n);
-    float *sum;
-    cudaMalloc(&sum, sizeof(float));
-    sumloss_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(div->accessSurface(), sum, n);
-    float *sum;
-    float cpu;
-    cudaMemcpy(&cpu, sum, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(sum);
-    return cpu;
-}
 
 __global__ void sumloss_kernel(CudaSurfaceAccessor<float> sufDiv, float *sum, unsigned int n){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
@@ -325,34 +255,6 @@ __global__ void rbgs_kernel(CudaSurfaceAccessor<float> sufPre, CudaSurfaceAccess
     float preNext = (pxp + pxn + pyp + pyn + pzp + pzn - div) * (1.f / 6.f);
     sufPre.write(preNext, x, y, z);
 }
-
-void smooth(CudaSurface<float> *v, CudaSurface<float> *f, unsigned int lev, int times = 4){
-    unsigned int tn = sizes[lev];
-    for (int step = 0; step < times; step++){
-        rbgs_kernel<0><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
-        rbgs_kernel<1><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
-    }
-}
-
-void vcycle(unsigned int lev, CudaSurface<float> *v, CudaSurface<float> *f){
-    if (lev >= sizes.size()){
-        unsigned int tn = sizes.back() / 2;
-        smooth(v, f, lev);
-        return;
-    }
-    auto *r = res[lev].get();
-    auto *r2 = res2[lev].get();
-    auto *e2 = err2[lev].get();
-    unsigned int tn = sizes[lev];
-    smooth(v, f, lev);
-    residual_kernel<<<dim3((tn+7)/8, (tn+7)/8, (tn+7)/8), dim3(8, 8, 8)>>>(r->accessSurface(), v->accessSurface(), f->accessSurface(), tn);
-    restrict_kernel<<<dim3((tn/2+7)/8, (tn/2+7)/8, (tn/2+7)/8), dim3(8, 8, 8)>>>(r2->accessSurface(), r->accessSurface(), tn/2);
-    fillzero_kernel<<<dim3((tn/2+7)/8, (tn/2+7)/8, (tn/2+7)/8), dim3(8, 8, 8)>>>(e2->accessSurface(), tn/2);    
-    vcycle(lev+1, e2, r2);
-    prolongate_kernel<<<dim3((tn/2+7)/8, (tn/2+7)/8, (tn/2+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), e2->accessSurface(), tn/2);    
-    smooth(v, f, lev);
-}
-
 
 __global__ void residual_kernel(CudaSurfaceAccessor<float> sufRes, CudaSurfaceAccessor<float> sufPre, CudaSurfaceAccessor<float> sufDiv, unsigned int n){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
@@ -392,15 +294,14 @@ __global__ void restrict_kernel(CudaSurfaceAccessor<float> sufPreNext, CudaSurfa
     sufPreNext.write(preNext, x, y, z);
 }
 
-
-__global__ void restrict_kernel(CudaSurfaceAccessor<float> sufPre, unsigned int n){
+__global__ void fillzero_kernel(CudaSurfaceAccessor<float> sufPre, unsigned int n) {
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
     int z = threadIdx.z + blockDim.z * blockIdx.z;
-    if (x >= n || y >=n || z>=n ) return;
-    sufPre.write(0.f, x, y, z);    
-}
+    if (x >= n || y >= n || z >= n) return;
 
+    sufPre.write(0.f, x, y, z);
+}
 
 __global__ void prolongate_kernel(CudaSurfaceAccessor<float> sufPreNext, CudaSurfaceAccessor<float> sufPre, unsigned int n){
     int x = threadIdx.x + blockDim.x * blockIdx.x;
@@ -410,7 +311,7 @@ __global__ void prolongate_kernel(CudaSurfaceAccessor<float> sufPreNext, CudaSur
     
     float preDelta = sufPre.read(x, y, z) * (0.5f / 8.f);
 #pragma unroll
-    for (int dz = 0; dz < 2; dz++}{
+    for (int dz = 0; dz < 2; dz++){
 #pragma unroll
         for (int dy = 0; dy < 2; dy++){
 #pragma unroll
@@ -422,6 +323,113 @@ __global__ void prolongate_kernel(CudaSurfaceAccessor<float> sufPreNext, CudaSur
         }
     }
 }
+
+
+
+
+
+struct SmokeSim : DisableCopy
+{
+    unsigned int n;
+    std::unique_ptr<CudaSurface<float4>> loc;
+    std::unique_ptr<CudaTexture<float4>> vel;
+    std::unique_ptr<CudaTexture<float4>> velNext;
+    std::unique_ptr<CudaTexture<float4>> clr;
+    std::unique_ptr<CudaTexture<float4>> clrNext;
+
+    std::unique_ptr<CudaSurface<float>> div;
+    std::unique_ptr<CudaSurface<float>> pre;
+    std::vector<std::unique_ptr<CudaSurface<float>>> res;
+    std::vector<std::unique_ptr<CudaSurface<float>>> res2;
+    std::vector<std::unique_ptr<CudaSurface<float>>> err2;
+    std::vector<unsigned int> sizes;
+
+    explicit SmokeSim(unsigned int _n, unsigned int _n0 = 16)
+    :n(_n), loc(std::make_unique<CudaSurface<float4>>(uint3{n, n, n}))
+    , vel(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
+    , velNext(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
+    , clr(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
+    , clrNext(std::make_unique<CudaTexture<float4>>(uint3{n, n, n}))
+    , div(std::make_unique<CudaSurface<float>>(uint3{n, n, n}))
+    , pre(std::make_unique<CudaSurface<float>>(uint3{n, n, n}))
+    {
+        unsigned int tn;
+        for (tn = n; tn >= _n0; tn /=2){
+            res.push_back(std::make_unique<CudaSurface<float>>(uint3{tn, tn, tn}));
+            res2.push_back(std::make_unique<CudaSurface<float>>(uint3{tn/2, tn/2, tn/2}));
+            err2.push_back(std::make_unique<CudaSurface<float>>(uint3{tn/2, tn/2, tn/2}));
+            sizes.push_back(tn);
+        }
+    }
+
+    void projection (int times = 400){
+        divergence_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(vel->accessSurface(), div->accessSurface(), n);
+
+        for (int step = 0; step < times; step++){
+            jacobi_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(div->accessSurface(), pre->accessSurface(), preNext->accessSurface(), n);
+            std::swap(pre, preNext);
+        }
+
+        subgradient_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(pre->accessSurface(), vel->accessSurface(), n);
+    }
+
+    float calc_loss(){
+        divergence_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(vel->accessSurface(), div->accessSurface(), n);
+        float *sum;
+        cudaMalloc(&sum, sizeof(float));
+        sumloss_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(div->accessSurface(), sum, n);
+        float *sum;
+        float cpu;
+        cudaMemcpy(&cpu, sum, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaFree(sum);
+        return cpu;
+    }
+
+
+    void smooth(CudaSurface<float> *v, CudaSurface<float> *f, unsigned int lev, int times = 4){
+        unsigned int tn = sizes[lev];
+        for (int step = 0; step < times; step++){
+            rbgs_kernel<0><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
+            rbgs_kernel<1><<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), f->accessSurface(), tn);
+        }
+    }
+
+    void vcycle(unsigned int lev, CudaSurface<float> *v, CudaSurface<float> *f){
+        if (lev >= sizes.size()){
+            unsigned int tn = sizes.back() / 2;
+            smooth(v, f, lev);
+            return;
+        }
+        auto *r = res[lev].get();
+        auto *r2 = res2[lev].get();
+        auto *e2 = err2[lev].get();
+        unsigned int tn = sizes[lev];
+        smooth(v, f, lev);
+        residual_kernel<<<dim3((tn+7)/8, (tn+7)/8, (tn+7)/8), dim3(8, 8, 8)>>>(r->accessSurface(), v->accessSurface(), f->accessSurface(), tn);
+        restrict_kernel<<<dim3((tn/2+7)/8, (tn/2+7)/8, (tn/2+7)/8), dim3(8, 8, 8)>>>(r2->accessSurface(), r->accessSurface(), tn/2);
+        fillzero_kernel<<<dim3((tn/2+7)/8, (tn/2+7)/8, (tn/2+7)/8), dim3(8, 8, 8)>>>(e2->accessSurface(), tn/2);    
+        vcycle(lev+1, e2, r2);
+        prolongate_kernel<<<dim3((tn/2+7)/8, (tn/2+7)/8, (tn/2+7)/8), dim3(8, 8, 8)>>>(v->accessSurface(), e2->accessSurface(), tn/2);    
+        smooth(v, f, lev);
+    }
+
+    void advection() {
+        advect_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(vel->accessTexture(), loc->accessSurface(), n);
+        resample_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(loc->accessSurface(), clr->accessTexture(), clrNext->accessSurface(), n);
+        resample_kernel<<<dim3((n+7)/8, (n+7)/8, (n+7)/8), dim3(8, 8, 8)>>>(loc->accessSurface(), vel->accessTexture(), velNext->accessSurface(), n);
+
+        std::swap(vel, velNext);
+        std::swap(clr, clrNext);
+    }
+
+    void step(int times = 16){
+        for (int step = 0; step < times; step++){
+            projection();
+            advection();
+        }
+    }
+
+};
 
 int main(){
     unsigned int n = 128;
