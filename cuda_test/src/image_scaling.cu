bbed5f7ba83bf6fbc8e51a2b1403428f355b7f78
mrzhuzhe <466648706@qq.com>
2023-03-17 21:45:32 +0800

    gear up

--- cuda_test/src/image_scaling.cu
+++ cuda_test/src/image_scaling.cu
@@ -7,7 +7,7 @@ float scale_factor,
 cudaTextureObject_t texObj
 ){
     const unsigned int tidX = blockIdx.x * blockDim.x + threadIdx.x;
-    const unsigned int tidY = blockIdx.x * blockDim.x + threadIdx.y;
+    const unsigned int tidY = blockIdx.y * blockDim.y + threadIdx.y;
     const unsigned index = tidY * scaled_width + tidX;
     imageScaledData[index] = tex2D<unsigned char>(texObj, (float)(tidX*scale_factor),(float)(tidY*scale_factor));
 }
