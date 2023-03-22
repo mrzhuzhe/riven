a5ef2d6f507f118d564cf64ed69a8fda2940461b
mrzhuzhe <466648706@qq.com>
2023-03-22 22:27:39 +0800

    conv

--- cuda_test/pattern/src/conv.cu
+++ cuda_test/pattern/src/conv.cu
@@ -4,6 +4,48 @@
 #include "common.h"
 
 #define BLOCK_DIM 16
+#define MAX_FILTER_LENGTH 128
+
+__global__ void conv_kernel_v1(float *d_out, float *d_in, float *d_filter, int n_r, int n_c, int filter_size)
+{
+    int i_x = blockDim.x * blockIdx.x + threadIdx.x;
+    int i_y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    float res = 0.f;
+    for (int f_row = -filter_size/2; f_row <= filter_size/2; ++f_row){
+        for (int f_col = -filter_size/2; f_col <= filter_size/2; ++f_col){
+            int img_r = id_y + f_row;
+            int img_c = id_x + f_col;
+            float img_v = (img_r >= 0 && img_r < n_row && img_c >=0; img_c < n_col) ? 
+                d_in[img_row * n_col + img_c] : 0.f;
+            float filter_v = d_filter[(f_row+filter_size/2)*filter_size + f_col + filter_size/2];
+            res += img_v * filter_v;
+        }
+    }
+
+    d_out[id_y * n_col + id_x] = res;
+}
+
+__constant__ float c_filter[MAX_FILTER_LENGTH*MAX_FILTER_LENGTH];
+__global__ void conv_kernel_v2(float *d_out, float *d_in, float *d_filter, int n_r, int n_c, int filter_size)
+{
+    int i_x = blockDim.x * blockIdx.x + threadIdx.x;
+    int i_y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    float res = 0.f;
+    for (int f_row = -filter_size/2; f_row <= filter_size/2; ++f_row){
+        for (int f_col = -filter_size/2; f_col <= filter_size/2; ++f_col){
+            int img_r = id_y + f_row;
+            int img_c = id_x + f_col;
+            float img_v = (img_r >= 0 && img_r < n_row && img_c >=0; img_c < n_col) ? 
+                d_in[img_row * n_col + img_c] : 0.f;
+            float filter_v = c_filter[(f_row+filter_size/2)*filter_size + f_col + filter_size/2];
+            res += img_v * filter_v;
+        }
+    }
+
+    d_out[id_y * n_col + id_x] = res;
+}
 
 int main(){
     return 0;
