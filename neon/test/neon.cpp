031487bc43c71be1dcc0a7e31c89dfaaa815edf3
Zhu Zhe 朱哲 <466648706@qq.com>
2023-03-30 22:07:05 +0800

    neon

--- neon/test/neon.cpp
+++ neon/test/neon.cpp
@@ -3,15 +3,15 @@
 #include "TikTok.h"
 #include <arm_neon.h>   //  https://developer.arm.com/documentation/dht0002/a/Introducing-NEON/Developing-for-NEON/Intrinsics?lang=en
 
-const int n = 10;
+const int n = 8 << 2;
 
 //int a[3*n];
 //int b[n];
 
-std::vector<int> a(n*3);
-std::vector<int> b(n);
+std::vector<uint8_t> a(n*3);
+std::vector<uint8_t> b(n);
 
-void fillArray(int n, int *src, int *dest){
+void fillArray(uint8_t n, uint8_t *src, uint8_t *dest){
     for (int i=0; i < n; i++){
         src[i*3] = 0+3*i;
         src[i*3+1] = 1+3*i;
@@ -20,7 +20,7 @@ void fillArray(int n, int *src, int *dest){
     }
 }
 
-void showArray(int n, int *src, int *dest){
+void showArray(uint8_t n, uint8_t *src, uint8_t *dest){
     printf("\n");
     for (int i=0; i < n; i++){
         int r = *src++;
@@ -30,7 +30,7 @@ void showArray(int n, int *src, int *dest){
     }
 }
 
-void gray_native(int n, int *src, int *dest){
+void gray_native(int n, uint8_t *src, uint8_t *dest){
     for (int i=0; i < n; i++){
         int r = *src++;
         int g = *src++;
@@ -48,7 +48,7 @@ void gray_native(int n, int *src, int *dest){
     }
 }
 
-void gray_uint8(int n, int *src, int *dest){
+void gray_uint8(int n, uint8_t *src, uint8_t *dest){
     for (int i=0; i < n; i++){
         uint8_t r = *src++;
         uint8_t g = *src++;
@@ -66,15 +66,22 @@ void gray_uint8(int n, int *src, int *dest){
     }
 }
 
-/*
-void gray_mla(int n, int *src, int *dest){
+
+//void gray_mla(int n, int *src, int *dest){
+void gray_mla(uint8_t *src, uint8_t *dest, int n){
+
+    //uint8x8_t _src = *src;
+
+
     n /= 8;
     uint8x8_t r_ratio = vdup_n_u8(77);
     uint8x8_t g_ratio = vdup_n_u8(151);
     uint8x8_t b_ratio = vdup_n_u8(28);
 
+    //printf("12123123 %d", *src);
+    ///*
     for (int i=0; i < n; i++){
-        uint8x8x3_t rgb = vld3_u8(src);
+        uint8x8x3_t rgb = vld3_u8(src);        
         uint8x8_t r = rgb.val[0];
         uint8x8_t g = rgb.val[1];
         uint8x8_t b = rgb.val[2];
@@ -84,21 +91,22 @@ void gray_mla(int n, int *src, int *dest){
         y = vmlal_u8(y, b, b_ratio);
         uint8x8_t ret = vshrn_n_u16(y, 8);
 
-        vstl_u8(dest, src);
+        vst1_u8(dest, ret);
         src += 3*8;
         dest +=8;
     }
+    //*/
 }
-*/
 
 int main(){
     //int *pa = a;
     //int *pb = b;
-    int *pa = a.data();
-    int *pb = b.data();
+    uint8_t *pa = a.data();
+    uint8_t *pb = b.data();
     fillArray(n, pa, pb);
     showArray(n, pa, pb);
    
+    
     Tik();
     gray_native(n, pa, pb);
     showArray(n, pa, pb);
@@ -110,12 +118,14 @@ int main(){
     Tok("uint8");
 
     Tik();
-    //gray_mla(n, pa, pb);
-    //showArray(n, pa, pb);
     float32x4_t v1 = { 1.0, 2.0, 3.0, 4.0 }, v2 = { 1.0, 1.0, 1.0, 1.0 };
     float32x4_t sum = vaddq_f32(v1, v2);
-    // => sum = { 2.0, 3.0, 4.0, 5.0 }
     std::cout << sum[0] << std::endl;
+    Tok("neon native");    
+
+    Tik();
+    gray_mla(pa, pb, n);
+    showArray(n, pa, pb);
     Tok("mal");
 
 
