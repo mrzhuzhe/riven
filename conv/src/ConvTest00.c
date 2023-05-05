100808a36f407b29d5a247f4409ce85dc2ee9404
mrzhuzhe <466648706@qq.com>
2023-05-05 16:02:15 +0800

    fix padding bug

--- conv/src/ConvTest00.c
+++ conv/src/ConvTest00.c
@@ -2,6 +2,11 @@
 #define C(i,j) c[ (j)*ldc + (i) ]
 #define KERNEL(i,j) kernel[ (j)*kw + (i) ]
 
+/*
+1. need padding 
+2. need bais
+*/
+
 void MY_MMult( int m,  int k,  double *a, int lda, 
                                     int kw, int kh, double *kernel,                                    
                                     double *c, int ldc )
@@ -9,8 +14,8 @@ void MY_MMult( int m,  int k,  double *a, int lda,
   int i, j, w, h;
   int Wo = m - kw + 1;
   int Ho = k - kh + 1;
-  for ( i=0; i< Wo; i++ ){
-      for ( j=0; j< Ho; j++ ){
+  for ( i=0; i<Wo; i++ ){
+      for ( j=0; j<Ho; j++ ){
         for (w = 0; w < kw; w++ ){
           for (h = 0; h < kh; h++){
             C( i,j ) = C( i,j ) + A( i + w, j + h) * KERNEL(w, h);          
--- conv/src/test_MMult.c
+++ conv/src/test_MMult.c
@@ -18,7 +18,7 @@ int main()
     p, 
     m, k,
     kw, kh,
-    lda, ldc, 
+    lda, 
     rep;
 
   double
@@ -35,47 +35,48 @@ int main()
   kh = 3;
 
   for ( p=PFIRST; p<=PLAST; p+=PINC ){
-  //for ( p=PFIRST; p<=PFIRST; p+=PINC ){
+  
     m = ( M == -1 ? p : M );
     k = ( K == -1 ? p : K );
 
     gflops = 2.0 * m * k * 1.0e-09;
 
     lda = ( LDA == -1 ? m : LDA );
-    ldc = ( LDC == -1 ? m : LDA );
 
     /* Allocate space for the matrices */
     /* Note: I create an extra column in A to make sure that
        prefetching beyond the matrix does not cause a segfault */
     a = ( double * ) malloc( lda * (k+1) * sizeof( double ) );     
     kernel = ( double * ) malloc( kw * kh * sizeof( double ) );      
-    c = ( double * ) malloc( ldc * (k+1) * sizeof( double ) );
-    cold = ( double * ) malloc( ldc * (k+1) * sizeof( double ) );
-    cref = ( double * ) malloc( ldc * (k+1) * sizeof( double ) );
+    c = ( double * ) malloc( lda * (k+1) * sizeof( double ) );
+    cold = ( double * ) malloc( lda * (k+1) * sizeof( double ) );
+    cref = ( double * ) malloc( lda * (k+1) * sizeof( double ) );
 
     /* Generate random matrices A, B, Cold */
     random_matrix( m, k, a, lda );
     random_matrix( kw, kh, kernel, kw );
+    //random_matrix( m, k, cold, lda );
 
-    random_matrix( m, k, cold, ldc );
-
-    copy_matrix( m, k, cold, ldc, cref, ldc );
+    //copy_matrix( m, k, cold, lda, cref, lda );
 
     /* Run the reference implementation so the answers can be compared */
 
-    REF_MMult( m, k, a, lda, kw, kh, kernel, cref, ldc );
-    //print_matrix(m, k, a, lda);
-    //print_matrix(kw, kh, kernel, kw);
-    //print_matrix(m, k, cref, ldc);
+    REF_MMult( m, k, a, lda, kw, kh, kernel, cref, lda );
     
+    /*
+    print_matrix(m, k, a, lda);
+    print_matrix(kw, kh, kernel, kw);
+    print_matrix(m, k, cref, lda);
+    */
+
     
     for ( rep=0; rep<NREPEATS; rep++ ){
-      copy_matrix( m, k, cold, ldc, c, ldc );
+      copy_matrix( m, k, cold, lda, c, lda );
 
       
       dtime = dclock();
 
-      MY_MMult( m, k, a, lda, kw, kh, kernel, c, ldc );
+      MY_MMult( m, k, a, lda, kw, kh, kernel, c, lda );
       
       dtime = dclock() - dtime;
 
@@ -85,7 +86,7 @@ int main()
 	dtime_best = ( dtime < dtime_best ? dtime : dtime_best );
     }
 
-    diff = compare_matrices( m, k, c, ldc, cref, ldc );
+    diff = compare_matrices( m, k, c, lda, cref, lda );
     
     printf( "%d %le %le \n", p, gflops / dtime_best, diff );
     fflush( stdout );
