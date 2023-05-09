01a08ac31dc1571a9aab9e4568695ffca2a4df3a
mrzhuzhe <466648706@qq.com>
2023-05-09 21:09:07 +0800

    warp primitive test

--- cuda_test/warp/src/test.cu
+++ cuda_test/warp/src/test.cu
@@ -1,11 +1,18 @@
 //  https://zhuanlan.zhihu.com/p/572820783 warp mask test
-
+//  https://zhuanlan.zhihu.com/p/75720006 more on cuda
 #include <iostream>
 #define N 128
 
 void init_data(float *data, int size){
     for (int i=0; i < size; i++ ){
         data[i] = i;
+    }
+}
+
+void init_data2(float *data, int size){
+    for (int i=0; i < size; i++ ){
+        //data[i] = i / 32;
+        data[i] = 0;
     }
 }
 
@@ -74,10 +81,14 @@ __global__ void test_kernel6(float *a, float *b, int n){
     b[tid] = __shfl_down_sync(0xffffffff, temp, 2); 
 }
 
-__device__ float warpSum(float val){
-    for (int i = warpSize/2; i > 0; i = i / 2){
-        unsigned int mask = __activemask();
-        val += __shfl_down_sync(mask, val, i); 
+#define FULL_MASK 0xffffffff
+__device__ float warpSum(float val, int n){
+    //unsigned mask = __ballot_sync(FULL_MASK, threadIdx.x < n);
+    if (threadIdx.x < n) { 
+        for (int i = warpSize/2; i > 0; i = i / 2){
+            unsigned int mask = __activemask();
+            val += __shfl_down_sync(mask, val, i); 
+        }
     }
     return val;
 }
@@ -88,9 +99,9 @@ __global__ void test_kernel7(float *a, float *b, int n){
     int warpid = threadIdx.x / 32;
     float val = a[tid];
     __shared__ float smm[32];
-    val = warpSum(val);
+    val = warpSum(val, n);
     if (lanid == 0){
-        smm[warpid] = warpSum(val); // smaller than 32 ?
+        smm[warpid] = warpSum(val, n); // smaller than 32 ?
     }
     __syncthreads();
     if (warpid == 0){
@@ -101,6 +112,38 @@ __global__ void test_kernel7(float *a, float *b, int n){
 }
 
 
+__global__ void test_kernel8(float *a, float *b, int n){   
+    int tid = threadIdx.x;
+    if (tid > n)
+        return;
+    float temp = a[tid];
+    b[tid] = __shfl_xor_sync(0xffffffff, temp, 3); 
+}
+
+
+
+__global__ void test_kernel9(float *a, float *b, int n){   
+    int tid = threadIdx.x;
+    if (tid > n)
+        return;
+    float temp = a[tid];
+    b[tid] = __match_any_sync(0xffffffff, temp); 
+}
+
+__global__ void test_kernel10(float *a, float *b, int n){   
+    int tid = threadIdx.x;
+    if (tid > n)
+        return;
+    float temp = a[tid];
+    int _pred = 0;
+    int *pred = &_pred;
+    //b[tid] = __match_all_sync(0xffffffff, temp, pred); // as same as match any    
+    __match_all_sync(0xffffffff, temp, pred); 
+    //printf(" %d ", *pred);
+    *pred = 1;
+    b[tid] = static_cast<float>(_pred);
+    
+}
 
 int main(){
     const int n_blocks = 1;
@@ -174,7 +217,7 @@ int main(){
 
     // test7
 
-    printf("\n --- shfl up reduce ---- ");
+    printf("\n --- shfl down reduce ---- ");
     test_kernel7<<<n_blocks, n_threads>>>(d_a, d_b, n);
     cudaMemcpy(h_b, d_b, m_size, cudaMemcpyDeviceToHost);
         
@@ -182,7 +225,34 @@ int main(){
     
     print_warp(h_b, size);
 
-
+    // test8
+    printf("\n --- shfl xor sync ---- ");
+    test_kernel8<<<n_blocks, n_threads>>>(d_a, d_b, n);
+    cudaMemcpy(h_b, d_b, m_size, cudaMemcpyDeviceToHost);
+        
+    cudaDeviceSynchronize();
     
+    print_warp(h_b, size);
+
+    // test9
+    printf("\n --- match any sync ---- ");
+    test_kernel9<<<n_blocks, n_threads>>>(d_a, d_b, n);
+    cudaMemcpy(h_b, d_b, m_size, cudaMemcpyDeviceToHost);
+        
+    cudaDeviceSynchronize();
+    
+    print_warp(h_b, size);
+
+    // test10
+    printf("\n --- match all sync ---- ");
+    init_data2(h_a, size);
+    print_warp(h_a, size);
+    test_kernel10<<<n_blocks, n_threads>>>(d_a, d_b, n);
+    cudaMemcpy(h_b, d_b, m_size, cudaMemcpyDeviceToHost);
+        
+    cudaDeviceSynchronize();
+    //??
+    print_warp(h_b, size);
+
     return 0;
 }
