003b7f5e188ebe9b82d01af7428d19b64dff9c8e
mrzhuzhe <466648706@qq.com>
2023-05-10 16:53:16 +0800

    seems not need libs

--- conv/src/ConvTest00_2_openmp.c
+++ conv/src/ConvTest00_2_openmp.c
@@ -2,6 +2,8 @@
 #define C(i,j) c[ (j)*lda + (i) ]
 #define KERNEL(i,j) kernel[ (j)*kw + (i) ]
 
+//#include <omp.h>
+
 void MY_MMult( int m,  int k,  double *a, int lda, 
                                     int kw, int kh, double *kernel,                                    
                                     double *c, int ldc, int stride )
@@ -11,7 +13,7 @@ void MY_MMult( int m,  int k,  double *a, int lda,
   int i, j, w, h;
   int Wo = (m - kw) / stride + 1;
   int Ho = (k - kh) / stride + 1;
-//#pragma omp parallel for 
+#pragma omp parallel for 
   for ( i=0; i< Wo; i+=1 ){
       for ( j=0; j< Ho; j+=1 ){
         for (w = 0; w < kw; w++ ){
