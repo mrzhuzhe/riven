cfddf52f7f45fa0171840ebeff73f0e51f4ca38e
mrzhuzhe <466648706@qq.com>
2023-05-17 22:35:56 +0800

    avx wrong

--- gemm/src/MMult22_avx.c
+++ gemm/src/MMult22_avx.c
@@ -21,9 +21,8 @@
 
 typedef union {
   __m256d v;
-  __m256d u;
   double d[4];
-} v4df_t
+} v4df_t;
 
 
 #define A(i,j) a[ (j)*lda + (i) ]
@@ -35,71 +34,87 @@ typedef union {
 void AddDot8x4(int k, double *a, int lda, double *b, int ldb, double *c, int ldc){
       
     int p;
-    v2df_t vc0010, vc0111, vc0212, vc0313, vc2030, vc2131, vc2232, vc2333;
-    v2df_t va01, va23;
-    v2df_t vb0p, vb1p, vb2p, vb3p; 
-    //double *b0p, *b1p, *b2p, *b3p;
+    v4df_t vc00102030, vc01112131, vc02122232, vc03132333, vc40506070, vc41516171, vc42526272, vc43536373;
+    v4df_t va0123, va4567;
+    v4df_t vb0p, vb1p, vb2p, vb3p; 
+
     
-    /*
-    b0p = &B( 0, 0 );
-    b1p = &B( 0, 1 );
-    b2p = &B( 0, 2 );
-    b3p = &B( 0, 3 );
-    */
-    vc0010.v = _mm_setzero_pd(); 
-    vc0111.v = _mm_setzero_pd(); 
-    vc0212.v = _mm_setzero_pd();
-    vc0313.v = _mm_setzero_pd();
-    vc2030.v = _mm_setzero_pd();
-    vc2131.v = _mm_setzero_pd();
-    vc2232.v = _mm_setzero_pd();
-    vc2333.v = _mm_setzero_pd();
+   
+    vc00102030.v = _mm256_setzero_pd(); 
+    vc01112131.v = _mm256_setzero_pd(); 
+    vc02122232.v = _mm256_setzero_pd();
+    vc03132333.v = _mm256_setzero_pd();
+    vc40506070.v = _mm256_setzero_pd();
+    vc41516171.v = _mm256_setzero_pd();
+    vc42526272.v = _mm256_setzero_pd();
+    vc43536373.v = _mm256_setzero_pd();
 
     for ( p=0; p<k; p++ ){
-      va01.v = _mm_load_pd((double *) a);      
-      va23.v = _mm_load_pd((double *) a+2);
+      va0123.v = _mm256_load_pd((double *) a);      
+      va4567.v = _mm256_load_pd((double *) a+4);
       
-      a += 4;
+      a += 8;
 
-      vb0p.v = _mm_loaddup_pd((double *) b);
-      vb1p.v = _mm_loaddup_pd((double *) (b + 1));
-      vb2p.v = _mm_loaddup_pd((double *) (b + 2));
-      vb3p.v = _mm_loaddup_pd((double *) (b + 3));
+      vb0p.v = _mm256_broadcast_sd((double *) b);
+      vb1p.v = _mm256_broadcast_sd((double *) (b + 1));
+      vb2p.v = _mm256_broadcast_sd((double *) (b + 2));
+      vb3p.v = _mm256_broadcast_sd((double *) (b + 3));
 
-      vc0010.v += va01.v * vb0p.v;                    
-      vc0111.v += va01.v * vb1p.v;          
-      vc0212.v += va01.v * vb2p.v; 
-      vc0313.v += va01.v * vb3p.v;   
+      vc00102030.v += va0123.v * vb0p.v;                    
+      vc01112131.v += va0123.v * vb1p.v;          
+      vc02122232.v += va0123.v * vb2p.v; 
+      vc03132333.v += va0123.v * vb3p.v;   
       //  _mm256_mul_pd _mm256_add_pd
-      vc2030.v += va23.v * vb0p.v;   
-      vc2131.v += va23.v * vb1p.v;   
-      vc2232.v += va23.v * vb2p.v;   
-      vc2333.v += va23.v * vb3p.v;        
+      vc40506070.v += va4567.v * vb0p.v;   
+      vc41516171.v += va4567.v * vb1p.v;   
+      vc42526272.v += va4567.v * vb2p.v;   
+      vc43536373.v += va4567.v * vb3p.v;        
       
-      b +=4;
+      b += 4;
 
     }
 
-    C(0, 0) += vc0010.d[0];
-    C(0, 1) += vc0111.d[0];
-    C(0, 2) += vc0212.d[0];
-    C(0, 3) += vc0313.d[0];
+    C(0, 0) += vc00102030.d[0];
+    C(0, 1) += vc01112131.d[0];
+    C(0, 2) += vc02122232.d[0];
+    C(0, 3) += vc03132333.d[0];
 
-    C(1, 0) += vc0010.d[1];
-    C(1, 1) += vc0111.d[1];
-    C(1, 2) += vc0212.d[1];
-    C(1, 3) += vc0313.d[1];
+    C(1, 0) += vc00102030.d[1];
+    C(1, 1) += vc01112131.d[1];
+    C(1, 2) += vc02122232.d[1];
+    C(1, 3) += vc03132333.d[1];
 
-    C(2, 0) += vc2030.d[0];
-    C(2, 1) += vc2131.d[0];
-    C(2, 2) += vc2232.d[0];
-    C(2, 3) += vc2333.d[0];
+    C(2, 0) += vc00102030.d[2];
+    C(2, 1) += vc01112131.d[2];
+    C(2, 2) += vc02122232.d[2];
+    C(2, 3) += vc03132333.d[2];
 
-    C(3, 0) += vc2030.d[1];
-    C(3, 1) += vc2131.d[1];
-    C(3, 2) += vc2232.d[1];
-    C(3, 3) += vc2333.d[1];
+    C(3, 0) += vc00102030.d[3];
+    C(3, 1) += vc01112131.d[3];
+    C(3, 2) += vc02122232.d[3];
+    C(3, 3) += vc03132333.d[3];
 
+////
+
+    C(4, 0) += vc40506070.d[0];
+    C(4, 1) += vc41516171.d[0];
+    C(4, 2) += vc42526272.d[0];
+    C(4, 3) += vc43536373.d[0];
+
+    C(5, 0) += vc40506070.d[1];
+    C(5, 1) += vc41516171.d[1];
+    C(5, 2) += vc42526272.d[1];
+    C(5, 3) += vc43536373.d[1];  
+
+    C(6, 0) += vc40506070.d[2];
+    C(6, 1) += vc41516171.d[2];
+    C(6, 2) += vc42526272.d[2];
+    C(6, 3) += vc43536373.d[2];
+
+    C(7, 0) += vc40506070.d[3];
+    C(7, 1) += vc41516171.d[3];
+    C(7, 2) += vc42526272.d[3];
+    C(7, 3) += vc43536373.d[3];
     
 };
 
@@ -112,6 +127,10 @@ void PackMatrixA(int k, double *a, int lda, double *a_to){
     *a_to++ = *(a_ij_pntr+1);
     *a_to++ = *(a_ij_pntr+2);
     *a_to++ = *(a_ij_pntr+3);
+    *a_to++ = *(a_ij_pntr+4);
+    *a_to++ = *(a_ij_pntr+5);
+    *a_to++ = *(a_ij_pntr+6);
+    *a_to++ = *(a_ij_pntr+7);
   }
 }
 
@@ -120,12 +139,19 @@ void PackMatrixB(int k, double *b, int ldb, double *b_to){
   int i;
   double 
     *b0p = &B(0, 0), *b1p = &B(0,1),
-    *b2p = &B(0, 2), *b3p = &B(0,3); 
+    *b2p = &B(0, 2), *b3p = &B(0,3),
+    *b4p = &B(0, 4), *b5p = &B(0,5),
+    *b6p = &B(0, 6), *b7p = &B(0,7); 
+
   for (i=0; i < k ; i++){    
     *b_to++ = *(b0p++);
     *b_to++ = *(b1p++);
     *b_to++ = *(b2p++);
     *b_to++ = *(b3p++);
+    *b_to++ = *(b4p++);
+    *b_to++ = *(b5p++);
+    *b_to++ = *(b6p++);
+    *b_to++ = *(b7p++);
   }
 }
 
@@ -139,9 +165,9 @@ void Innerkernel(int m, int n, int k, double *a, int lda, double *b, int ldb, do
       PackMatrixB(k, &B(0, j), ldb, &packedB[j*k]);
     }
     
-    for (i = 0; i < m; i +=4){
+    for (i = 0; i < m; i +=8){
       if ( j == 0 ) PackMatrixA(k, &A(i, 0), lda, &packedA[i*k]);
-      AddDot8x4(k, &packedA[i*k], 4, &packedB[j*k], k , &C( i,j ), ldc);
+      AddDot8x4(k, &packedA[i*k], 8, &packedB[j*k], k , &C( i,j ), ldc);
     }
   }
 }
