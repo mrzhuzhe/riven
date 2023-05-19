b929ad8116e3d926fd005ea130ed17ce2e4fe039
mrzhuzhe <466648706@qq.com>
2023-05-19 19:03:30 +0800

    reorder is funning

--- gemm/src/MMult23_reordering.c
+++ gemm/src/MMult23_reordering.c
@@ -1,10 +1,4 @@
-// 4 X 4 tiles
-// SIMD
-// still has variable bad case 
-// add kernel pack
-//  First, we pack the block of A so that we march through it contiguously.
-
-
+// load data while add or multi
 
 /* Create macros so that the matrices are stored in column-major order */
 
@@ -39,65 +33,109 @@ void AddDot8x4(int k, double *a, int lda, double *b, int ldb, double *c, int ldc
     register v4df_t vc00102030, vc01112131, vc02122232, vc03132333, vc40506070, vc41516171, vc42526272, vc43536373;
     register v4df_t va0123, va4567;
     register v4df_t vb0p, vb1p, vb2p, vb3p; 
-
+    register v4df_t temp1, temp2, temp3, temp4;
     
+
+    va0123.v = _mm256_load_pd((double *) a);      
+    va4567.v = _mm256_load_pd((double *) a+4);     
    
     vc00102030.v = _mm256_setzero_pd(); 
     vc01112131.v = _mm256_setzero_pd(); 
     vc02122232.v = _mm256_setzero_pd();
     vc03132333.v = _mm256_setzero_pd();
+
+    vb0p.v = _mm256_broadcast_sd((double *) b);
+    vb1p.v = _mm256_broadcast_sd((double *) (b + 1));
+    vb2p.v = _mm256_broadcast_sd((double *) (b + 2));
+    vb3p.v = _mm256_broadcast_sd((double *) (b + 3));
+
     vc40506070.v = _mm256_setzero_pd();
     vc41516171.v = _mm256_setzero_pd();
     vc42526272.v = _mm256_setzero_pd();
-    vc43536373.v = _mm256_setzero_pd();
+    vc43536373.v = _mm256_setzero_pd();    
 
     for ( p=0; p<k; p++ ){
+          
+      temp1.v = _mm256_mul_pd(va0123.v, vb0p.v);     
+      temp2.v = _mm256_mul_pd(va0123.v, vb1p.v);          
+      temp3.v = _mm256_mul_pd(va0123.v, vb2p.v); 
+      temp4.v = _mm256_mul_pd(va0123.v, vb3p.v);   
+      // load next va0123
+      va0123.v = _mm256_load_pd((double *) a+8);  
+      // add
+      vc00102030.v = _mm256_add_pd(vc00102030.v, temp1.v);
+      vc01112131.v = _mm256_add_pd(vc01112131.v, temp2.v);
+      vc02122232.v = _mm256_add_pd(vc02122232.v, temp3.v);
+      vc03132333.v = _mm256_add_pd(vc03132333.v, temp4.v);
 
-      //  数据需要对其
-      //  https://stackoverflow.com/questions/33373318/avx-segmentation-fault-on-linux
-      // unalign will suddenly fail but align will be a little slower
-      va0123.v = _mm256_load_pd((double *) a);      
-      va4567.v = _mm256_load_pd((double *) a+4);
-
-      //va0123.v = _mm256_loadu_pd((double *) a);      
-      //va4567.v = _mm256_loadu_pd((double *) a+4);
+      // load new data before add
+      temp1.v = _mm256_mul_pd(va4567.v, vb0p.v);  
+      temp2.v = _mm256_mul_pd(va4567.v, vb1p.v);  
+      temp3.v = _mm256_mul_pd(va4567.v, vb2p.v);  
+      temp4.v = _mm256_mul_pd(va4567.v, vb3p.v);
+      // load next va4567 
+      va4567.v = _mm256_load_pd((double *) a+12); 
       
-      a += 8;
-
-      vb0p.v = _mm256_broadcast_sd((double *) b);
-      vb1p.v = _mm256_broadcast_sd((double *) (b + 1));
-      vb2p.v = _mm256_broadcast_sd((double *) (b + 2));
-      vb3p.v = _mm256_broadcast_sd((double *) (b + 3));
-
-      //vc00102030.v += va0123.v * vb0p.v;                    
-      vc00102030.v += _mm256_mul_pd(va0123.v, vb0p.v);     
-      vc01112131.v += _mm256_mul_pd(va0123.v, vb1p.v);          
-      vc02122232.v += _mm256_mul_pd(va0123.v, vb2p.v); 
-      vc03132333.v += _mm256_mul_pd(va0123.v, vb3p.v);   
-      //  _mm256_mul_pd _mm256_add_pd
-      vc40506070.v += _mm256_mul_pd(va4567.v, vb0p.v);  
-      vc41516171.v += _mm256_mul_pd(va4567.v, vb1p.v);  
-      vc42526272.v += _mm256_mul_pd(va4567.v, vb2p.v);  
-      vc43536373.v += _mm256_mul_pd(va4567.v, vb3p.v);     
-
       b += 4;
 
+      // add 
+      vc40506070.v = _mm256_add_pd(vc40506070.v, temp1.v);
+      vb0p.v = _mm256_broadcast_sd((double *) b);
+      vc41516171.v = _mm256_add_pd(vc41516171.v, temp2.v);
+      vb1p.v = _mm256_broadcast_sd((double *) (b + 1));
+      vc42526272.v = _mm256_add_pd(vc42526272.v, temp3.v);
+      vb2p.v = _mm256_broadcast_sd((double *) (b + 2));
+      vc43536373.v = _mm256_add_pd(vc43536373.v, temp4.v);
+      vb3p.v = _mm256_broadcast_sd((double *) (b + 3));
+
+      a += 8;
+      
     }
 
     //
+    temp1.v = _mm256_load_pd(c);
+    vc00102030.v = _mm256_add_pd(temp1.v, vc00102030.v);
+    _mm256_store_pd(c, vc00102030.v);    
+    
+    temp2.v = _mm256_load_pd((c + ldc));
+    vc01112131.v = _mm256_add_pd(temp2.v, vc01112131.v);
+    _mm256_store_pd((c + ldc), vc01112131.v);    
+    
+    temp3.v = _mm256_load_pd((c + ldc * 2));
+    vc02122232.v = _mm256_add_pd(temp3.v, vc02122232.v);
+    _mm256_store_pd((c + ldc * 2), vc02122232.v);    
+
+    temp3.v = _mm256_load_pd((c + ldc * 3));
+    vc03132333.v = _mm256_add_pd(temp3.v, vc03132333.v);
+    _mm256_store_pd((c + ldc * 3), vc03132333.v);    
+    /*
     _mm256_store_pd(c, _mm256_add_pd(_mm256_load_pd(c), vc00102030.v));
-    _mm256_store_pd((c + ldc), _mm256_add_pd(_mm256_load_pd((c + ldc)), vc01112131.v));
+    _mm256_store_pd(, _mm256_add_pd(_mm256_load_pd((c + ldc)), vc01112131.v));
     _mm256_store_pd((c + ldc * 2), _mm256_add_pd(_mm256_load_pd((c + ldc * 2)), vc02122232.v));
     _mm256_store_pd((c + ldc * 3), _mm256_add_pd(_mm256_load_pd((c + ldc * 3)), vc03132333.v));
-    
+    */
 
-    //
+    temp1.v = _mm256_load_pd(c + 4);
+    vc40506070.v = _mm256_add_pd(temp1.v, vc40506070.v);
+    _mm256_store_pd(c+4 , vc40506070.v);    
+    
+    temp2.v = _mm256_load_pd((c + ldc + 4));
+    vc41516171.v = _mm256_add_pd(temp2.v, vc41516171.v);
+    _mm256_store_pd((c + ldc + 4), vc41516171.v);    
+    
+    temp3.v = _mm256_load_pd((c + ldc * 2 + 4));
+    vc42526272.v = _mm256_add_pd(temp3.v, vc42526272.v);
+    _mm256_store_pd((c + ldc * 2 + 4), vc42526272.v);    
+
+    temp3.v = _mm256_load_pd((c + ldc * 3 + 4));
+    vc43536373.v = _mm256_add_pd(temp3.v, vc43536373.v);
+    _mm256_store_pd((c + ldc * 3 + 4), vc43536373.v); 
+    /*    
     _mm256_store_pd((c + 4), _mm256_add_pd(_mm256_load_pd((c + 4)), vc40506070.v));
     _mm256_store_pd((c + ldc + 4), _mm256_add_pd(_mm256_load_pd((c + ldc + 4)), vc41516171.v));
     _mm256_store_pd((c + ldc * 2 + 4), _mm256_add_pd(_mm256_load_pd((c + ldc * 2 + 4)), vc42526272.v));
-    _mm256_store_pd((c + ldc * 3 + 4), _mm256_add_pd(_mm256_load_pd((c + ldc * 3 + 4)), vc43536373.v));
-
-         
+    _mm256_store_pd((c + ldc * 3 + 4), _mm256_add_pd(_mm256_load_pd((c + ldc * 3 + 4)), vc43536373.v));  
+    */       
 
 };
 
