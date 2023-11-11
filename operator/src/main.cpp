d4319f04118af63329b94fd370d2a5c02695b9c7
mrzhuzhe <466648706@qq.com>
2023-11-11 22:50:03 +0800

    lazy evaluate test

--- operator/src/main.cpp
+++ operator/src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+class A {    
+    public:
+        A (int n){
+            value = n;
+        }
+        int value = 1;
+        int operator+(int n){
+            std::cout << "add " << value + n << std::endl;
+            return value + n;
+        }
+};
+
 int main(){
-    
+    A a(2);
+    int b;
+    b = a+1;
+    std::cout << " b " << b << std::endl;
     return 0;
 }
