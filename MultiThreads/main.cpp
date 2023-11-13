895b4c1ca1e294b6e5c9c12a3991a514c906aa36
mrzhuzhe <466648706@qq.com>
2023-11-13 22:44:10 +0800

    g++ and clang is different

--- MultiThreads/main.cpp
+++ MultiThreads/main.cpp
@@ -25,13 +25,13 @@ class CB : public CA {
 int CB::n = 321;
 
 void inc_a(std::string name){
-    // std::lock_guard<std::mutex> guard(mutex_lock);
     a++;
+    std::lock_guard<std::mutex> guard(mutex_lock);
     std::cout << name << " " << a << std::endl;
 }
 
 int main(){
-
+    // todo not work in g++
     #if defined(__has_feature)
     #  if __has_feature(thread_sanitizer)
     // code that builds only under ThreadSanitizer
@@ -44,7 +44,7 @@ int main(){
     std::thread td_1(inc_a, "a1");
     std::thread td_2(inc_a, "b2");
     {
-        // std::lock_guard<std::mutex> guard(mutex_lock);
+        std::lock_guard<std::mutex> guard(mutex_lock);
         std::cout << "main thread " << a << std::endl;
     }
 
