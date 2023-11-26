377fc25ef58e6c3dbe6f646bd007c5713ce85995
mrzhuzhe <466648706@qq.com>
2023-11-26 13:22:26 +0800

    pthread create

--- MultiThreads/main.cpp
+++ MultiThreads/main.cpp
@@ -2,6 +2,8 @@
 #include <thread>
 #include <mutex>
 #include <string>
+#include <pthread.h>
+#include <cstring>
 
 //thread_local 
 static int a = 1;
@@ -24,29 +26,43 @@ class CB : public CA {
 
 int CB::n = 321;
 
-class CC {
-    private:
-        CC(){
-            std::cout << "CC create" << std::endl;
-        }
-        static CC* impl;
-    public: 
-        int nx;
-        CC(const CC& other) = delete;
-        static CC* getInstance(){
-            if (impl == nullptr) {
-                impl = new CC();
-            }
-            return impl;
-        };
-};
+// class CC {
+//     private:
+//         CC(){
+//             std::cout << "CC create" << std::endl;
+//         }
+//         static CC* impl;
+//     public: 
+//         int nx;
+//         CC(const CC& other) = delete;
+//         static CC* getInstance(){
+//             if (impl == nullptr) {
+//                 impl = new CC();
+//             }
+//             return impl;
+//         };
+// };
 
-CC* CC::getInstance() = nullptr;
+// CC* CC::getInstance() = nullptr;
 
 void inc_a(std::string name){
     a++;
     std::lock_guard<std::mutex> guard(mutex_lock);
     std::cout << name << " " << a << std::endl;
+}
+
+
+//  https://man7.org/linux/man-pages/man3/pthread_create.3.html
+struct thread_info{
+    pthread_t thread_id;
+    int thread_num;
+    int arg_string;    
+};
+
+void* thread_fn(void *arg){
+    struct  thread_info *tinfo  = (thread_info *)arg;    
+    std::cout << "thread fn " << tinfo->arg_string << std::endl;
+    return (void*)tinfo;
 }
 
 int main(){
@@ -74,7 +90,36 @@ int main(){
     cb.n = 333;
     cb.getN();
 
-    CC* cc = CC::getInstance();
+    // CC* cc = CC::getInstance();
     //std::cout << "singleton data " <<  cc->nx << std::endl;
+
+    int status;
+    void* res;
+    size_t num_threads = 2;
+    pthread_attr_t attr;
+    thread_info tinfo[num_threads];
+
+    status = pthread_attr_init(&attr);
+
+    for (size_t tnum = 0; tnum < num_threads; tnum++){
+        tinfo[tnum].thread_num = tnum;
+        //char str[20] = strcat("asdasdsadasd", (char*)tnum);
+        tinfo[tnum].arg_string = 12 + tnum;
+        status = pthread_create(&tinfo[tnum].thread_id, &attr, &thread_fn, &tinfo[tnum]);
+        if (status) {
+            std::cout << "create status " << status << std::endl;
+        }
+    }
+
+    status = pthread_attr_destroy(&attr);
+
+    for (size_t tnum = 0; tnum < num_threads; tnum++){
+        status = pthread_join(tinfo[tnum].thread_id, &res);
+        if (status) {
+            std::cout << "join " << tinfo[tnum].thread_id << " "  << status << std::endl;
+        }
+        std::cout << "joined" << ((thread_info *)res)->thread_num << std::endl;
+    }
+    
     return 0;
 }
