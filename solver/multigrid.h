7526fccc97e2528ebf0f3083f2ef56c946cc0463
mrzhuzhe <466648706@qq.com>
2024-02-20 22:33:15 +0800

    multigrid method

---
+++ solver/multigrid.h
@@ -0,0 +1,5 @@
+/*
+0. a very good code https://people.sc.fsu.edu/~jburkardt/cpp_src/poisson_1d_multigrid/poisson_1d_multigrid.html
+1. https://www10.cs.fau.de/publications/reports/TechRep_2008-03.pdf
+2. https://github.com/ddemidov/amgcl
+*/
