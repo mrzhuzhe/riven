732042a413745bdc596fbd268836c8e30346e902
mrzhuzhe <466648706@qq.com>
2024-02-24 21:32:54 +0800

    interpolate wrong

--- solver/test_ieration.cpp
+++ solver/test_ieration.cpp
@@ -4,23 +4,10 @@
 #include "jacobian.h"
 #include "multigrid.h"
 
-int main() {
-    int rows = 128, cols = 128;
-    Eigen::MatrixXf mat01(rows, cols);
+
+void test_case(Eigen::MatrixXf mat01, int rows, int cols){
     Eigen::MatrixXf b(rows, 1);
     Eigen::MatrixXf x(rows, 1);
-    
-    // mat01 = Eigen::MatrixXf::Random(rows, cols);
-    // mat01 = mat01.transpose() * mat01;  // easy to solve positive sysmetric
-    
-    Tridiagonal_mat(mat01, rows, cols);
-    std::cout << "\n mat01.block(0, 0, 8, 8) \n" << mat01.block(0, 0, 8, 8) << std::endl;
-
-    // mat01 << 10, -1 , 2, 0, 
-    // -1, 11, -1,3,
-    // 2, -1, 10,-1,
-    // 0, -3, -1, 8;
-    // b << 6, 25, -11, 15;
     b = Eigen::MatrixXf::Random(rows, 1);
     jacobian_solver(mat01, x, b, rows, cols);
     
@@ -45,7 +32,7 @@ int main() {
     Cmat2Fmat(mat01_8, mat01_16, rows/2, cols/2);    // this interpolate is not good
     //std::cout << "\n mat01_16 \n" << mat01_16 << std::endl;
     
-    std::cout << "\n------------------------------ V multi grid" << std::endl;
+    std::cout << "\n V multi grid" << std::endl;
     //  16 -> 8 -> 4 -> 8 -> 16 
     Eigen::MatrixXf x_16(rows, 1);
     Eigen::MatrixXf x_8(rows/2, 1);
@@ -56,24 +43,50 @@ int main() {
     Fmat2Cmat(b_8, b_4, rows/2, 1);    
 
     // 16 - 8
-    jacobian_solver(mat01, x_16, b, rows, cols, 1e-2f);    
+    jacobian_solver(mat01, x_16, b, rows, cols, 1e-3f);    
     Fmat2Cmat(x_16, x_8, rows, 1);
 
     // 8-4
-    jacobian_solver(mat01_8, x_8, b_8, rows/2, cols/2, 1e-3f);
+    jacobian_solver(mat01_8, x_8, b_8, rows/2, cols/2, 1e-4f);
     Fmat2Cmat(x_8, x_4, rows/2, 1);
     
     // 4
-    jacobian_solver(mat01_4, x_4, b_4, rows/4, cols/4, 1e-4f);
+    jacobian_solver(mat01_4, x_4, b_4, rows/4, cols/4, 1e-5f);
     
     // 4-8
     Cmat2Fmat(x_4, x_8, rows/4, 1);
-    jacobian_solver(mat01_8, x_8, b_8, rows/2, cols/2, 1e-5f);
+    jacobian_solver(mat01_8, x_8, b_8, rows/2, cols/2, 1e-6f);
 
     // 8 - 16
     Cmat2Fmat(x_8, x_16, rows/2, 1);
     jacobian_solver(mat01, x_16, b, rows, cols);
 
-    std::cout << "multi grid error" << (x_16 - x2).maxCoeff() << " " << (x_16 - x2).minCoeff() << std::endl;
+    std::cout << "multi grid error " << (x_16 - x2).maxCoeff() << " " << (x_16 - x2).minCoeff() << std::endl;
+}
+
+int main() {
+    int rows = 96, cols = 96;
+    Eigen::MatrixXf mat01(rows, cols);
+    Tridiagonal_mat(mat01, rows, cols);
+    std::cout << "\n ------------------------------ test case 1 mat01.block(0, 0, 8, 8) \n" 
+    << mat01.block(0, 0, 8, 8) 
+    << " \n rows cols " << rows << " " << cols << std::endl;
+
+    // mat01 << 10, -1 , 2, 0, 
+    // -1, 11, -1,3,
+    // 2, -1, 10,-1,
+    // 0, -3, -1, 8;
+    // b << 6, 25, -11, 15;
+    test_case(mat01, rows, cols);
+
+    Eigen::MatrixXf mat02(rows, cols);
+    mat02 = Eigen::MatrixXf::Random(rows, cols);
+    mat02 = mat02.transpose() * mat02;  // easy to solve positive sysmetric
+    std::cout << "\n ------------------------------ test case2 mat02.block(0, 0, 8, 8) \n" 
+    << mat02.block(0, 0, 8, 8) 
+    << " \n rows cols " << rows << " " << cols << std::endl;
+
+    test_case(mat02, rows, cols);
+
     return 0;
 }
