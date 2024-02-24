133318311dec0f59c01724c4792cd4bf960f3937
mrzhuzhe <466648706@qq.com>
2024-02-24 16:53:20 +0800

    bad

--- solver/multigrid.h
+++ solver/multigrid.h
@@ -3,3 +3,53 @@
 1. https://www10.cs.fau.de/publications/reports/TechRep_2008-03.pdf
 2. https://github.com/ddemidov/amgcl
 */
+
+void ctof ( int nc, double uc[], int nf, double uf[] )
+{
+  int ic;
+  int iff;
+
+  for ( ic = 0; ic < nc; ic++ )
+  {
+    iff = 2 * ic;
+    uf[iff] = uf[iff] + uc[ic];
+  }
+
+  for ( ic = 0; ic < nc - 1; ic++ )
+  {
+    iff = 2 * ic + 1;
+    uf[iff] = uf[iff] + 0.5 * ( uc[ic] + uc[ic+1] );
+  }
+
+  return;
+}
+
+void ftoc ( int nf, double uf[], double rf[], int nc, double uc[], 
+  double rc[] )
+{
+  int ic;
+  int iff;
+
+  for ( ic = 0; ic < nc; ic++ )
+  {
+    uc[ic] = 0.0;
+  }
+
+  rc[0] = 0.0;
+  for ( ic = 1; ic < nc - 1; ic++ )
+  {
+    iff = 2 * ic;
+    rc[ic] = 4.0 * ( rf[iff] + uf[iff-1] - 2.0 * uf[iff] + uf[iff+1] );
+  }
+  rc[nc-1] = 0.0;
+
+  return;
+}
+
+void FtoC(const Eigen::MatrixXf& F_mat, const Eigen::MatrixXf& C_mat, int rows, int cols){
+
+}
+
+void CtoF(const Eigen::MatrixXf& C_mat, const Eigen::MatrixXf& F_mat, int rows, int cols){
+
+}
--- solver/test_ieration.cpp
+++ solver/test_ieration.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Dense>
 #include <limits.h>
 #include "jacobian.h"
+#include "multigrid.h"
 
 int main() {
     int rows = 16, cols = 16;
@@ -24,6 +25,35 @@ int main() {
     Eigen::MatrixXf x2(rows, 1);
     x2 = mat01.colPivHouseholderQr().solve(b);
     std::cout << "eigen solution\n" << x2 << std::endl;
+
+    // Eigen::MatrixXf mat01_16(rows/2, cols/2);
+    // Eigen::MatrixXf mat01_8(rows/4, cols/4);
+    // Eigen::MatrixXf mat01_4(rows/8, cols/8);
+    // FtoC(mat01, mat01_16, rows, cols);    
+    int nc = 8;
+    int nf = 16;
+    double uc[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    double uf[] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1, 11.11, 12.12, 13.13, 14.14, 15.15 };
+    double rf[] = {11.1, 12.2, 13.3, 14.4, 15.5, 16.6, 17.7, 18.8, 19.9, 110.1, 111.11, 112.12, 113.13, 114.14, 115.15 };
+    double uf2c[nf];
+    double uc2f[nf];
+    double rc[nf];
+    ftoc (nf+1, uf, rf, nc+1, uf2c, rc);
+
+    // for (int i=0; i < nf; i++){
+    //      std::cout <<  uf2c[i] << " ";
+    // }
+    std::cout <<  std::endl;
+    for (int i=0; i < nf; i++){
+         std::cout <<  rc[i] << " ";
+    }
+    std::cout <<  std::endl;
+
+    ctof( nc+1, uc, nf, uc2f);
+    std::cout <<  std::endl;
+    for (int i=0; i < nf; i++){
+        std::cout <<  uc2f[i] << " ";
+    }
 
     return 0;
 }
